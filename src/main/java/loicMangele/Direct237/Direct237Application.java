c1dc3a709637788fa8052541e16cef9f0cf817a3
MangeleLoic <behonloic@gmail.com>
2025-01-18 11:43:28 +0100

    first commit

---
+++ src/main/java/loicMangele/Direct237/Direct237Application.java
@@ -0,0 +1,13 @@
+package loicMangele.Direct237;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Direct237Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Direct237Application.class, args);
+	}
+
+}
---
+++ src/test/java/loicMangele/Direct237/Direct237ApplicationTests.java
@@ -0,0 +1,13 @@
+package loicMangele.Direct237;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Direct237ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
